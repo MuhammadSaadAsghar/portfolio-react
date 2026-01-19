23b6aee18925045b5ae336c1fc2e10e37c0d96e4
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-01-19 15:45:00 +0500

    create navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Navbar.css"
+import phone from '../../assets/phone.png'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+<nav className='navbar'>
+    <div >
+    <ul className='nav-icon'>
+        <li>
+    <i className="fa-solid fa-envelope"></i>
+    <span className='email'>rsaad6421@gmail.com</span>
+    </li>
+    <li>
+    <img src={phone} alt="phone" />
+    <span className='phone'>03161642540</span>
+    </li>
+</ul>
+</div>
+<div className='social-icon'>
+<span><i className="fa-brands fa-facebook-f"></i></span>
+<span><i className="fa-brands fa-twitter"></i></span>
+<span><i className="fa-brands fa-instagram"></i></span>
+<span><i className="fa-brands fa-pinterest"></i></span>
+</div>
+
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Components/Navbar/Navbarbottom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='container'>
+
+    </div>
+  )
+}
+
+export default navbar
